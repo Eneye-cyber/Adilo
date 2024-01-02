325c8a4dd1f8b1e36c967dc4a01155b413806bef
Eneye <alaoeneye@gmail.com>
2024-01-02 15:15:42 +0100

    initial

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__container">
+      <div class="header__logo flex centered">
+        <div class="header__logo-image flex centered"><div></div></div>
+        <h2 class="header__logo-text ">Adilo</h2>
+      </div>
+
+      <nav class="header__nav flex">
+        <ul class="flex item-center w-full">
+          <li><a class="nav-link" href="#">Projects</a></li>
+          <li>
+            <a class="nav-link item-center active" href="#">Tools & App 
+              <span class="material-symbols-outlined">expand_more</span>
+            </a>
+          </li>
+          <li><a class="nav-link" href="#">Channels</a></li>
+          <li><a class="nav-link" href="#">Contacts</a></li>
+          <li><a class="nav-link" href="#">Analytics</a></li>
+          <li><a class="nav-link" href="#">Settings</a></li>
+        </ul>
+      </nav>
+
+      <div class="header__profile flex item-center">
+        <a class="header__profile-link text-lg" href="#">Help</a>
+        <div class="divider"></div>
+        <div class="flex item-center">
+          <div class="header__profile-image">
+
+          </div>
+          <div>
+            <p>Cody Fisher</p>
+            <p>CodyFisher@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <aside class="sidebar">
+    <ul>
+      <li>
+        <a class="sidebar__link item-center w-full text-md active" href="#">
+          <img src="../assets/icon/video-player.svg" alt="video" width="25px" height="24px">
+          <p>My Recordings</p>
+        </a>
+        <a class="sidebar__link item-center w-full text-md" href="#">
+          <span class="material-symbols-outlined"> share</span>
+          <p>Requested</p>
+        </a>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<style scoped>
+</style>
